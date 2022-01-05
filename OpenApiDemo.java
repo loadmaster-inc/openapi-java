0fbe517a0bb45e110b0e5d95a80cbddd308838cb
david <david@zhuangxiang.com>
2022-01-05 22:50:01 +0800

    添加注释

--- OpenApiDemo.java
+++ OpenApiDemo.java
@@ -4,20 +4,20 @@ import org.json.simple.parser.JSONParser;
 
 public class OpenApiDemo {
     public static void main(String[] args) {
-        HttpRequest response = HttpRequest.post("https://api.zhuangxiang.com/connect/token")
-                .header("Cookie", "Abp.TenantId={your tenant id}")
-                .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
-                .send("client_id={your app id}&client_secret={your app secret}&grant_type=password&username={your account name(admin)}&password={your password}");
-        JSONParser parser = new JSONParser();
+        HttpRequest response = HttpRequest.post("https://api.zhuangxiang.com/connect/token") // URL
+                .header("Cookie", "Abp.TenantId={your tenant id}") // 设置Cookie，使用租户id
+                .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8") // 设置HTTP Body类型
+                .send("client_id={your app id}&client_secret={your app secret}&grant_type=password&username={your account name(admin)}&password={your password}"); // 填写app-id、app-secret、账号及密码，然后发送请求
+        JSONParser parser = new JSONParser(); // Json解析器
         String access_token = null;
         try {
-            access_token = (String) (((JSONObject) (parser.parse(response.body()))).get("access_token"));
+            access_token = (String) (((JSONObject) (parser.parse(response.body()))).get("access_token")); // 从响应中获取access_token
         } catch (Exception e) {
             e.printStackTrace();
         }
-        JSONObject taskdata = new JSONObject();
+        JSONObject taskdata = new JSONObject(); // 构造要计算的数据
         taskdata.put("type", 0);
-        JSONObject cargo = new JSONObject();
+        JSONObject cargo = new JSONObject(); // 货物数据
         cargo.put("name", "cargo1");
         cargo.put("length", 1.1);
         cargo.put("width", 0.8);
@@ -27,7 +27,7 @@ public class OpenApiDemo {
         JSONArray cargoes = new JSONArray();
         cargoes.add(cargo);
         taskdata.put("packingCargoes", cargoes);
-        JSONObject container = new JSONObject();
+        JSONObject container = new JSONObject(); // 容器数据
         container.put("name", "20ft");
         container.put("InnerX", 2.35);
         container.put("InnerY", 2.38);
@@ -35,7 +35,7 @@ public class OpenApiDemo {
         container.put("Maxload", 21000);
         JSONArray containers = new JSONArray();
         containers.add(container);
-        taskdata.put("packingContainers", containers);
+        taskdata.put("packingContainers", containers); 
         taskdata.put("interimContainers", new JSONArray());
         taskdata.put("loadingOptions", new JSONObject());
         taskdata.put("interimOptions", new JSONObject());
@@ -46,7 +46,7 @@ public class OpenApiDemo {
         inputs.put("taskData", taskdata);
         response = HttpRequest.post("https://openapi.zhuangxiang.com/OptimizeLoadingTask")
                 .header("Authorization", "bearer " + access_token).header("content-type", "application/json")
-                .send(inputs.toJSONString());
+                .send(inputs.toJSONString()); // 发送请求，使用access_token进行认证，Body为要计算的数据
         System.out.println(response.body());
     }
 }
